f63768161779b4bf9526de4b3ce94180d0abb187
Alexander von Dollen <alexander.d.vondollen.civ@army.mil>
2025-02-16 17:22:17 -0500

    configured cpplint

--- dot_config/nvim/lua/utilities/nvim-lint.lua
+++ dot_config/nvim/lua/utilities/nvim-lint.lua
@@ -4,8 +4,21 @@ return {
   config = function()
 
     require('lint').linters_by_ft = {
-      cpp = {'cpplint'},
+      cpp = {
+        'cpplint',
+      }
     }
+
+    -- Register the cpplint linter with multiple filters
+    require('lint').linters.cpplint.args = {
+      '--filter=-legal/copyright,\
+-whitespace,\
+-build/header_guard,\
+-readability/namespace,\
+-runtime/indentation_namespace,\
+-readability/alt_tokens'
+    }
+
   end,
 
   vim.keymap.set("n", "<leader>l", function()
